bfe95f2d66c65f8c63918837133d17d0a0476995
Haris Muslić <39027423+thewhitewolf2411@users.noreply.github.com>
2021-02-05 12:38:03 +0100

    creation of sales lot db

---
+++ app/Eloquent/SalesLot.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Eloquent;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SalesLot extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'sales_lot';
+
+
+    
+
+    
+}
