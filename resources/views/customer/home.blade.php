70aa1a284cfa5fe685b2f4b1a3ba832739a6242e
zuxbrt <33069559+zuxbrt@users.noreply.github.com>
2021-04-12 15:52:45 +0100

    home page rework in progress

--- resources/views/customer/home.blade.php
+++ resources/views/customer/home.blade.php
@@ -34,13 +34,13 @@
     </div>
 
     <div class="home-element how-container">
-        <div class="center-title-container">
-            <p>Sounds great, but, <br> how does it actually work?</p>
+        <div class="text-center">
+            <p class="large-bold-text">Sounds great, but, <br> how does it actually work?</p>
         </div>
 
-        <div class="how-buttons-container">
+        <div class="how-buttons-container mt-5">
             <div class="how-button-container active" id="selling-btn" onclick="changeHowState('selling')">
-                <p>Selling</p>
+                <p class="semilarge-bold-text">Selling</p>
             </div>
         </div>
 
@@ -48,10 +48,10 @@
             <div class="how-second-text-container">
                 <div class="how-text-element">
                     <div class="how-text-title-container">
-                        <p>2. FREE DELIVERY OPTIONS</p>
+                        <p class="regular-bold-text">2. FREE DELIVERY OPTIONS</p>
                     </div>
                     <div class="how-text-container">
-                        <p>Once you have completed your sales order, you simply request for a free sales pack or print your own labels to send in your device.</p>
+                        <p class="regular-text">Once you have completed your sales order, you simply request for a free sales pack or print your own labels to send in your device.</p>
                     </div>
                 </div>
             </div>
@@ -75,18 +75,18 @@
             <div class="how-first-text-container">
                 <div class="how-text-element">
                     <div class="how-text-title-container">
-                        <p>1. REGISTER YOUR DEVICE TO SELL</p>
+                        <p class="regular-bold-text">1. REGISTER YOUR DEVICE TO SELL</p>
                     </div>
                     <div class="how-text-container">
-                        <p>Find your old device and how much it's worth. Choose your preferred payment option with all your details.</p>
+                        <p  class="regular-text">Find your old device and how much it's worth. Choose your preferred payment option with all your details.</p>
                     </div>
                 </div>
-                <div class="how-text-element">
+                <div class="how-text-element move-bottom">
                     <div class="how-text-title-container">
-                        <p>3. FAST SAME DAY PAYMENT</p>
+                        <p class="regular-bold-text">3. FAST SAME DAY PAYMENT</p>
                     </div>
                     <div class="how-text-container">
-                        <p>When we receive your device, we will check  it against your order. If it is all correct, payment will be made on the same day of receipt. Woohoo!</p>
+                        <p class="regular-text">When we receive your device, we will check  it against your order. If it is all correct, payment will be made on the same day of receipt. Woohoo!</p>
                     </div>
                 </div>
             </div>
