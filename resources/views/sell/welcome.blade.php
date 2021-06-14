01a6c13522a2caae6c4a71ee881d512ba0b80b63
zuxbrt <33069559+zuxbrt@users.noreply.github.com>
2021-06-14 08:26:26 +0100

    font sizing fixes

--- resources/views/sell/welcome.blade.php
+++ resources/views/sell/welcome.blade.php
@@ -131,19 +131,19 @@
             <div class="selling-info-items-container">
                 <div class="selling-info-item">
                     <img class="selling-info-img" src="{{asset('/sell_images/image-1.svg')}}">
-                    <p class="selling-info-bold-text mt-4">SAME DAY PAYMENT</p>
+                    <p class="selling-info-bold-text">SAME DAY PAYMENT</p>
                 </div>
                 <div class="selling-info-item">
                     <img class="selling-info-img" src="{{asset('/sell_images/image-2.svg')}}">
-                    <p class="selling-info-bold-text mt-4">FREE POSTAGE</p>
+                    <p class="selling-info-bold-text">FREE POSTAGE</p>
                 </div>
                 <div class="selling-info-item">
                     <img class="selling-info-img" src="{{asset('/sell_images/image-3.svg')}}">
-                    <p class="selling-info-bold-text mt-4">DATA IS ALWAYS<br> PROTECTED</p>
+                    <p class="selling-info-bold-text">DATA IS ALWAYS<br> PROTECTED</p>
                 </div>
                 <div class="selling-info-item">
                     <img class="selling-info-img" src="{{asset('/sell_images/image-4.svg')}}">
-                    <p class="selling-info-bold-text mt-4">BAMBOO PRICE OR YOUR<br> DEVICE BACK FREE</p>
+                    <p class="selling-info-bold-text">BAMBOO PRICE OR YOUR<br> DEVICE BACK FREE</p>
                 </div>
             </div>
 
