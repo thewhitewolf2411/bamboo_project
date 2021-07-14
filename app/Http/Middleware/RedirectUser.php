e20fefd33e3f23cca8ddcb4ae3503a0a772297c1
zuxbrt <33069559+zuxbrt@users.noreply.github.com>
2021-07-14 14:32:19 +0100

    Update RedirectUser.php
    restrict ips in progress

--- app/Http/Middleware/RedirectUser.php
+++ app/Http/Middleware/RedirectUser.php
@@ -16,10 +16,26 @@ class RedirectUser
      */
     public function handle($request, Closure $next)
     {
+        $allowed = [
+            "217.34.54.192",
+            "109.170.152.167",
+            "217.138.25.114",
+            "192.168.9.43"
+        ];
         // redirect to login if not authorized
         if(!Auth::user()){
             request()->session()->flash('nouser', true);
             return redirect('/userprofile');
+        } else {
+            $ip = $request->ip();
+            if(!in_array($ip, $allowed)){
+                //dd(Auth::user());
+                if(Auth::user()){
+                    Auth::logout();
+                    // $this->guard()->logout();
+                }
+                return redirect('/');
+            }
         }
         
         return $next($request);
